74779cd3becaef6ba373ab0df63519868789227c
James Troughton <jamesstroughton@gmail.com>
2020-10-20 16:20:07 +1300

    created react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="app">
+    <div className="app__container">
+      <p>Hello</p>
+    </div>
+  </div>;
+}
+
+export default App;
