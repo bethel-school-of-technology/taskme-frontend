3849adb05e50587c14ab24cdb6986299a3637893
Michelle Huber <9modes@gmail.com>
2020-11-01 18:46:28 -0700

    added frontend/backend integration for tasks

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../Styles/Tasks.css';
 
 function Tasks() {
+
+    var [isEdit, setIsEdit] = useState("");
+    var [tasks, setTasks] = useState("");
+    var [task, setTask] = useState("");
+
+    var [taskName, setTaskName] = useState("");
+    var [completed, setCompleted] = useState(false);
+
+    useEffect(() => {
+        const getAllTasks = async () => {
+            let tasksData = await fetch("http://localhost:3000/tasks/")
+            let t = await tasksData.json();
+
+            //console.log(t);
+
+            setTasks(t.data.tasks);
+        }
+
+        getAllTasks();
+    }, [])
+
     return (
         <div className='tasks'>
             <h3>Tasks</h3>
+            {tasks.map((task, idx) => {
+                return(
+                    <div key={idx}>
+                        {task.tasks.taskname} {task.tasks.completed}
+                    </div>
+                )
+            })}
         </div>
     )
 };
